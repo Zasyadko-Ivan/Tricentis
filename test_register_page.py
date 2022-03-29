7cda42be90a0fec16f020a71f7a0dc50cc2c6a19
ivanzasadko <yyeee31@gmail.com>
2022-03-29 08:14:09 +0300

    Added new tests email and password

--- test_register_page.py
+++ test_register_page.py
@@ -1,5 +1,6 @@
 import time
 from .pages.register_page import RegisterPage
+import pytest
 
 link = 'http://demowebshop.tricentis.com/register'
 
@@ -12,6 +13,42 @@ def test_successful_registration_of_a_new_user(browser):
     l_name = "Zasyadko"
     email = str(time.time()) + "@fakemail.org"
     password = "ggga!Q24"
+    confirm_password = password
+    page = RegisterPage(browser, link)
+    page.open()
+    page.choose_gender_male()
+    page.fill_in_the_main_registration_fields(f_name, l_name, email, password, confirm_password)
+    page.click_button_register()
+    page.should_not_be_verification_not_entered_email_already_exists()
+    page.verification_of_successful_registration()
+
+
+@pytest.mark.parametrize('email', [str(time.time()) + "@fakemail.org",
+                                  str(time.time()) + "@gmail.org",
+                                  str(time.time()) + "ashfur" + "@fakemail.org"])
+def test_successful_registration_of_a_new_user_different_email(browser, email):
+    browser.implicitly_wait(5)
+    f_name = "Ivan"
+    l_name = "Zasyadko"
+    password = "ggga!Q24"
+    confirm_password = password
+    page = RegisterPage(browser, link)
+    page.open()
+    page.choose_gender_male()
+    page.fill_in_the_main_registration_fields(f_name, l_name, email, password, confirm_password)
+    page.click_button_register()
+    page.should_not_be_verification_not_entered_email_already_exists()
+    page.verification_of_successful_registration()
+
+
+@pytest.mark.parametrize('password', ['Super@!21',
+                                  '12345fhga',
+                                  'ny_naprimer_takoi_12'])
+def test_successful_registration_of_a_new_user_different_password(browser, password):
+    browser.implicitly_wait(5)
+    f_name = "Ivan"
+    l_name = "Zasyadko"
+    email = str(time.time()) + "@fakemail.org"
     confirm_password = password
     page = RegisterPage(browser, link)
     page.open()
