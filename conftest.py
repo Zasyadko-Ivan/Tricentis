302a1d15e979af2294cc064dba11ae31b731758b
ivanzasadko <yyeee31@gmail.com>
2022-04-03 18:14:17 +0300

    Added mod headless

--- conftest.py
+++ conftest.py
@@ -1,6 +1,6 @@
 import pytest
-from selenium.webdriver.common.by import By
 from selenium import webdriver
+
 chrome_link = "driver/chromedriver/chromedriver"
 firefox_link = "driver/geckodriver/geckodriver"
 
@@ -8,18 +8,29 @@ firefox_link = "driver/geckodriver/geckodriver"
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--headless', action='store', default='no',
+                     help="Choose headless mod")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    headless = request.config.getoption("headless")
     browser = None
-    if browser_name == "chrome":
+    if browser_name == "chrome" and headless == "no":
         print("\nstart chrome browser for test..")
         browser = webdriver.Chrome(executable_path=chrome_link)
-    elif browser_name == "firefox":
+    elif browser_name == "firefox" and headless == "no":
         print("\nstart firefox browser for test..")
         browser = webdriver.Firefox(executable_path=firefox_link)
+    elif browser_name == "chrome" and headless == "no":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome(executable_path=chrome_link)
+    elif browser_name == "firefox" and headless == "yes":
+        print("\nstart firefox browser for test..")
+        options = webdriver.FirefoxOptions()
+        options.add_argument("--headless")
+        browser = webdriver.Firefox(options=options, executable_path=firefox_link)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
